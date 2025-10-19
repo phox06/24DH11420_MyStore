535aa4e2f3db01a33f18f09f872fa1800155be31
Phu Chau minh <chauminhphu23@gmail.com>
2025-10-19 22:25:45 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24DH11420_LTTH_BE234.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24DH11420_LTTH_BE234.MvcApplication" Language="C#" %>
